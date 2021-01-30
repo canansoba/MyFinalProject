10697c885e8cd220c3e0d9a7c2ae47387cb7c5f8
Canan <Canan@ASUS>
2021-01-31 00:04:47 +0300

    Add project files.

---
+++ Entities/Abstract/IEntity.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.Abstract
+{
+    //veri tabanı nesnesi
+    //IEntity implement eden class bir veritabanı tablosudur
+    public interface IEntity
+    {
+    }
+}
