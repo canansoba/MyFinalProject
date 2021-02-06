e4e8fed1bcd230ae704e4ed57974ac6a4b152aea
Canan <Canan@ASUS>
2021-02-06 23:55:59 +0300

    06.02.2021

--- DataAccess/Abstract/IProductDal.cs
+++ DataAccess/Abstract/IProductDal.cs
@@ -1,5 +1,6 @@
 ﻿using Core.DataAccess;
 using Entities.Concrete;
+using Entities.DTOs;
 using System;
 using System.Collections.Generic;
 using System.Text;
@@ -9,7 +10,7 @@ namespace DataAccess.Abstract
     //interface'in operasyonları public
     public interface IProductDal:IEntityRepository<Product>
     {
-        
+        List<ProductDetailDto> GetProductDetails();
     }
 }
 
